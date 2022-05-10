77f4d1b92f02716f4e806a84d76835e346562d65
staaph <contact.staaph@gmail.com>
2022-05-10 15:23:19 +0200

    added Calendar

---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import VueCalendar from '@/components/VueCalendar.vue';
+</script>
+
+<template>
+  <main class="flex items-center justify-center h-screen">
+    <VueCalendar />
+  </main>
+</template>
