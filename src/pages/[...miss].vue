45f072aa2852d3a57434dd25b5cf074ea10ebc8d
staaph <contact.staaph@gmail.com>
2022-05-30 13:59:53 +0200

    added 404 page

---
+++ src/pages/[...miss].vue
@@ -0,0 +1,13 @@
+<template>
+  <wrapper>
+    <main class="h-full w-full grid place-content-center dark:text-white">
+      <h1 class="text-3xl font-medium">404</h1>
+      <p>The page you are looking for cannot be found</p>
+      <div class="grid place-content-center p-2">
+        <button class="bg-gray-200 rounded p-2 text-black mt-6">
+          <router-link to="/">Back home</router-link>
+        </button>
+      </div>
+    </main>
+  </wrapper>
+</template>
